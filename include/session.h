d11226475161b9edea5cf24686c3b93a696cf0b7
724789975 <724789975@qq.com>
2022-08-26 02:41:26 +0800

    数据定义
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ include/session.h
@@ -0,0 +1,16 @@
+#ifndef __SESSION_H__
+#define __SESSION_H__
+
+namespace FXNET
+{
+	class CSession
+	{
+	public:
+	private:
+		/* data */
+	};
+
+
+}
+
+#endif //!__SESSION_H__
---
+++ include/udp_session.h
@@ -0,0 +1,12 @@
+#include "session.h"
+
+#ifndef __UDP_SESSION_H__
+#define __UDP_SESSION_H__
+
+namespace FXNET
+{
+
+
+}
+
+#endif	//!__UDP_SESSION_H__
