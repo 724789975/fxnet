1477232e5def22219efb66db5b4a714b6888c341
724789975 <724789975@qq.com>
2022-08-28 18:34:34 +0800

    功能添加

---
+++ include/sliding_window_def.h
@@ -0,0 +1,37 @@
+#ifndef __SLIDING_WINDOW_H__
+#define __SLIDING_WINDOW_H__
+
+/**
+ * @brief 发送缓存帧数量
+ */
+#define TCP_SEND_WINDOW_SIZE 32
+/**
+ * @brief 发送缓存帧长度
+ */
+#define TCP_SEND_WINDOW_BUFF_SIZE 512
+/**
+ * @brief 接收缓存帧数量
+ */
+#define TCP_RECV_WINDOW_SIZE 32
+/**
+ * @brief 接收缓存帧长度
+ */
+#define TCP_RECV_WINDOW_BUFF_SIZE 512
+/**
+ * @brief 发送缓存帧数量
+ */
+#define UDP_SEND_WINDOW_SIZE 32
+/**
+ * @brief 发送缓存帧长度
+ */
+#define UDP_SEND_WINDOW_BUFF_SIZE 512
+/**
+ * @brief 接收缓存帧数量
+ */
+#define UDP_RECV_WINDOW_SIZE 32
+/**
+ * @brief 接收缓存帧长度
+ */
+#define UDP_RECV_WINDOW_BUFF_SIZE 512
+
+#endif	//! __SLIDING_WINDOW_H__
