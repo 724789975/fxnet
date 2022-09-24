400449a721210db4f16a2a1bdc2e43944533b2d5
724789975 <724789975@qq.com>
2022-09-25 00:41:28 +0800

    功能完善
    Signed-off-by: 724789975 <724789975@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "common/buff_contral.h"
 #include "common/cas_lock.h"
+#include "common/iothread.h"
 
 int main()
 {
 	FXNET::BufferContral<> buffcontral;
 	FXNET::CCasLock l;
+
+	FXNET::FxIoModule::CreateInstance();
 }
