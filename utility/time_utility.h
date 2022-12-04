fca53e9e91c099e9ab2ea233085458e4d645e16a
724789975 <724789975@qq.com>
2022-12-04 23:04:26 +0800

    效用库

--- utility/time_utility.h
+++ utility/time_utility.h
@@ -14,38 +14,38 @@ namespace UTILITY
 {
 	class TimeUtility : public TSingleton<TimeUtility>
 	{
-
-	};
-	static unsigned long long GetTimeUS( void )
-	{
+		unsigned long long GetTimeUS( void )
+		{
 #ifdef _WIN32
-		// 从1601年1月1日0:0:0:000到1970年1月1日0:0:0:000的时间(单位100ns)
+			// 从1601年1月1日0:0:0:000到1970年1月1日0:0:0:000的时间(单位100ns)
 #define EPOCHFILETIME   (116444736000000000UL)
-		FILETIME ft;
-		LARGE_INTEGER li;
-		unsigned long long qwTime = 0;
-		GetSystemTimeAsFileTime(&ft);
-		li.LowPart = ft.dwLowDateTime;
-		li.HighPart = ft.dwHighDateTime;
-		// 从1970年1月1日0:0:0:000到现在的微秒数(UTC时间)
-		qwTime = (li.QuadPart - EPOCHFILETIME) /10;
-		return qwTime;
+			FILETIME ft;
+			LARGE_INTEGER li;
+			unsigned long long qwTime = 0;
+			GetSystemTimeAsFileTime(&ft);
+			li.LowPart = ft.dwLowDateTime;
+			li.HighPart = ft.dwHighDateTime;
+			// 从1970年1月1日0:0:0:000到现在的微秒数(UTC时间)
+			qwTime = (li.QuadPart - EPOCHFILETIME) /10;
+			return qwTime;
 #else
-		timeval tv;
-		gettimeofday(&tv, 0);
-		return (unsigned long long)tv.tv_sec * 1000000 + (int64_t)tv.tv_usec;
+			timeval tv;
+			gettimeofday(&tv, 0);
+			return (unsigned long long)tv.tv_sec * 1000000 + (int64_t)tv.tv_usec;
 #endif
-}
+		}
 
-	static unsigned long long GetTimeMS( void )
-	{
-		return GetTimeUS() / 1000;
-	}
+		unsigned long long GetTimeMS( void )
+		{
+			return this->GetTimeUS() / 1000;
+		}
+		
+		unsigned long long GetTime( void )
+		{
+			return this->GetTimeMS() / 1000;
+		}
+	};
 	
-	static unsigned long long GetTime( void )
-	{
-		return GetTimeMS() / 1000;
-	}
 };
 
 #endif	//!__GetTime_H__
