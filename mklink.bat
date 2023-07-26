c72e72ab52927b4b64f9f55ec76af72635a7c37c
724789975 <724789975@qq.com>
2023-07-26 17:05:49 +0800

    修改硬链接脚本

--- mklink.bat
+++ mklink.bat
@@ -1,9 +1,13 @@
 set target_path=%1%
 md %target_path%
 set cur_path=%~dp0
-for /r %%i in (*.cpp, *.h, *.mak, Makefile) do (
-   set file_path=%%~dfi
+for /r %%i in (*Makefile*, *.cpp, *.h, *.mak) do (
+   set file_path=%%i
    set dir_path=%%~dpi
+   echo %file_path%
+   echo %%i
+
+   REM dir %file_path%
 
    setlocal enabledelayedexpansion
 
@@ -13,8 +17,10 @@ for /r %%i in (*.cpp, *.h, *.mak, Makefile) do (
 
    set "target_file=!file_path:%cur_path%=\!"
    echo %target_path%!target_file!
+   echo %file_path%
 
-   mklink /h %target_path%!target_file! %file_path%
+   mklink /h %target_path%!target_file! %%i
+   endlocal
    )
 
 
