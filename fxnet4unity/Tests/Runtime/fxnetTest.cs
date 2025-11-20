cc62baebdb8fda8079afe29c888853c06e3f94ef
724789975 <724789975@qq.com>
2025-11-20 15:40:02 +0800

    修改初始化方式

--- fxnet4unity/Tests/Runtime/fxnetTest.cs
+++ fxnet4unity/Tests/Runtime/fxnetTest.cs
@@ -9,7 +9,9 @@ public class NewBehaviourScript : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+#if !UNITY_EDITOR
 		DLLImport.StartIOModule();
+#endif
 		DLLImport.SetLogCallback(delegate (byte[] pData, int dwLen)
 		{
 			Debug.Log(pData);
