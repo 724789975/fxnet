30c2910eb3c273280490442d49689e052b7f60a6
724789975 <724789975@qq.com>
2025-11-24 18:19:10 +0800

    修复重复创建module问题

--- fxnet4unity/Tests/Runtime/fxnetTest.cs
+++ fxnet4unity/Tests/Runtime/fxnetTest.cs
@@ -9,9 +9,7 @@ public class NewBehaviourScript : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-#if !UNITY_EDITOR
 		DLLImport.StartIOModule();
-#endif
 		DLLImport.SetLogCallback(delegate (byte[] pData, int dwLen)
 		{
 			Debug.Log(pData);
