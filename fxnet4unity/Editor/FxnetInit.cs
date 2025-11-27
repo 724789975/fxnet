93da7a25af866e8bf61572db85d57b67fb532705
724789975 <724789975@qq.com>
2025-11-27 23:37:23 +0800

    调整dll加载问题

--- fxnet4unity/Editor/FxnetInit.cs
+++ fxnet4unity/Editor/FxnetInit.cs
@@ -6,10 +6,7 @@ using fxnetlib.dllimport;
 using UnityEditor;
 public class FxnetInit
 {
-	[InitializeOnLoadMethod]
 	static void InitializeOnLoadMethod()
 	{
-		DLLImport.StartIOModule();
-		UnityEngine.Debug.Log("FxnetInit.InitializeOnLoadMethod");
 	}
 }
